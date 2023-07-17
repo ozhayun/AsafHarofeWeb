b456e55df0b9e05f161ed9f2852431404179fedf
Oz Hayun <ozhayun2@gmail.com>
2023-07-17 15:49:38 +0300

    Add Facebook, instagram and youtube icons and links

--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -4,6 +4,9 @@ import vetLionImagePath from "../Public/HomePage/VetLion.png";
 import vetPandaImagePath from "../Public/HomePage/VetPanda.png";
 import vetMonkeyImagePath from "../Public/HomePage/VetMonkey.png";
 import vetDogImagePath from "../Public/HomePage/VetDog.png";
+import youtubeImagePath from "../Public/HomePage/YouTube.png";
+import facebookImagePath from "../Public/HomePage/Facebook.png";
+import instagramImagePath from "../Public/HomePage/Instagram.png";
 
 const HomePage = () => {
     return (
@@ -17,6 +20,17 @@ const HomePage = () => {
             </div>
             <button id="MedicalDocumentsButton">מסמכים רפואים</button>
             <button id="StartGameButton">התחל משחק</button>
+            <div id='linksDiv'>
+                <a href="https://www.youtube.com/@Aharofeh" target="_blank">
+                    <img src={youtubeImagePath} id="YoutubeIcon" alt='Youtube'/>
+                </a>
+                <a href="https://www.facebook.com/Shamir.medical/about" target="_blank">
+                    <img src={facebookImagePath} id="FacebookIcon" alt='Facebook'/>
+                </a>
+                <a href="https://instagram.com/shamir.medical.center?igshid=MTIzZWMxMTBkOA==" target="_blank">
+                    <img src={instagramImagePath} id="InstagramIcon" alt='Instagram'/>
+                </a>
+            </div>
         </div>
     )
 }
