4cb58cc29cfe465b10d80b7653a2b34a3106ab90
Oz Hayun <ozhayun2@gmail.com>
2023-07-17 14:25:30 +0300

    Create home page with logos

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,21 @@ import './App.css'
 
 function App() {
   return (
-    <div>
-      <div id={"Title"}>Hello Asaffff Ha Rofe</div>
-      <p>Hello Salit!!!</p>
-      <p>Hello Oz!!</p>
+    <div id="Body">
+        <img src='./Public/HomePage/ShamirLogo.png' id="ShamirLogo" alt="Shamir"/>
+        <div className="AnimalsBackground">
+            <img src='./Public/HomePage/אריה.png' id="LionBackground" alt="Lion"/>
+            <img src='./Public/HomePage/פנדוקטור.png' id="PandaBackground" alt="Panda"/>
+            <img src='./Public/HomePage/קופרסור.png' id="MonkeyBackground" alt="Monkey"/>
+            <img src='./Public/HomePage/כלבטרינר.png' id="DogBackground" alt="Dog"/>
+        </div>
+        <button id="MedicalDocumentsButton">מסמכים רפואים</button>
+        <button id="StartGameButton">התחל משחק</button>
     </div>
   )
 }
 
 export default App
+
+
+
