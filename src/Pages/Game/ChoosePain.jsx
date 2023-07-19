c3c82168b1bb09d5fe73ab2cf7d18a140eb9f6bd
salit-av <salit.avramovich@gmail.com>
2023-07-19 15:38:27 +0300

    start working on ChoosePain

--- src/Pages/Game/ChoosePain.jsx
+++ src/Pages/Game/ChoosePain.jsx
@@ -1,9 +1,76 @@
-import react from 'react'
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import { Link } from 'react-router-dom';
+import HomeIcon from '@mui/icons-material/Home.js';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import Stack from '@mui/material/Stack';
+import shamirLogoPath from '../../../Public/HomePage/ShamirLogo.png';
+import {Label} from "@mui/icons-material";
+import Typography from "@mui/material/Typography";
+import ThermometerImagePath from "../../../Public/HomePage/Thermometer.png";
+import PlasterImagePath from "../../../Public/HomePage/Plaster.png";
+import {useState} from "react";
+
 const ChoosePain = () => {
-    return(
-        <div>Choose Pain</div>
-    )
+    function appBarLabel() {
+        return (
+            <Toolbar sx={{ maxWidth: 390, display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+                <div>
+                    <IconButton
+                        id="HomeButton"
+                        component={Link}
+                        to="/"
+                        aria-label="homePage"
+                        sx={{ marginRight: 'auto' }}
+                    >
+                        <HomeIcon />
+                    </IconButton>
+                </div>
+                <div>
+                    {/* Adjust the 'width' and 'height' properties to change the image size */}
+                    <img src={shamirLogoPath} id="ShamirLogo" alt="Shamir" style={{ width: '20%', height: '20%', marginRight: 'auto' }} />
+                </div>
+            </Toolbar>
+        );
+    }
+
+    const darkTheme = createTheme({
+        palette: {
+            mode: 'dark',
+            primary: {
+                main: '#1976d2',
+            },
+        },
+    });
+
+    const [selectedAnimal, setSelectedPain] = useState("");
+
+    const handlePainClick = (pain) => {
+        setSelectedPain(pain);
+    }
+
+    return (
+        <div>
+            <Stack spacing={2} sx={{ flexGrow: 1 }}>
+                    <AppBar position="static" color="primary" enableColorOnDark>
+                        {appBarLabel()}
+                    </AppBar>
+                <Typography id = "Title" variant="h2" gutterBottom>
+                    מה כואב לחיה?
+                </Typography>
+                <div className='rectanglePain'>
+                    <button className="piece cut" onClick={() => handlePainClick('cut')} data-selected={selectedAnimal === 'dog' ? 'true' : 'false'}>
+                        <img src={ThermometerImagePath} style={{width: "100%", height: "100%"}} alt="Dog" />
+                    </button>
+                    <button className="piece fiver" onClick={() => handlePainClick('fiver')} data-selected={selectedAnimal === 'lion' ? 'true' : 'false'}>
+                        <img src={PlasterImagePath} style={{width: "100%", height: "100%"}} alt="Lion" />
+                    </button>
+                </div>
+            </Stack>
+        </div>
+    );
 }
 
-
-export default ChoosePain
+export default ChoosePain;
