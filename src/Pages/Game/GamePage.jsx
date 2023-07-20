cdb078e344d1cca254faea26c48a6f00ff63fd21
Oz Hayun <ozhayun2@gmail.com>
2023-07-20 13:42:09 +0300

    Create GamePage.css and create grid, toolbar and title

--- src/Pages/Game/GamePage.jsx
+++ src/Pages/Game/GamePage.jsx
@@ -1,8 +1,20 @@
+import './GamePage.css';
+import CustomToolbar from "../../Components/CustomToolbar.jsx";
+import * as React from "react";
+
 const GamePage = () => {
-return (
-    <div>Game!</div>
-  )
-}
+    return (
+        <div className="container">
+            <div className="toolbar-container">
+                <CustomToolbar toolbarTitle="סולמות ומגלשות"/>
+            </div>
+            <div className="game-board">
+                {Array.from({ length: 100 }, (_, index) => (
+                    <div className="cell" key={index} />
+                ))}
+            </div>
+        </div>
+    );
+};
 
-
-export default GamePage
+export default GamePage;
