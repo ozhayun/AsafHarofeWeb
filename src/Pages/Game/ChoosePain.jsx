73d0a0b6d52669754242cf44f1b35cdbd3882b5c
salit-av <salit.avramovich@gmail.com>
2023-07-21 01:24:21 +0300

    Adding a line of color according to the animal on the game board

--- src/Pages/Game/ChoosePain.jsx
+++ src/Pages/Game/ChoosePain.jsx
@@ -1,5 +1,5 @@
-import './ChoosePain.css'
 import * as React from 'react';
+import './ChoosePain.css';
 import Typography from "@mui/material/Typography";
 import ThermometerImagePath from "../../../Public/HomePage/Thermometer.png";
 import PlasterImagePath from "../../../Public/HomePage/Plaster.png";
@@ -8,6 +8,7 @@ import CustomToolbar from '../../Components/CustomToolbar.jsx';
 import {Button} from "@mui/material";
 import {useNavigate} from "react-router";
 import {useLocation} from "react-router-dom";
+
 
 const ChoosePain = () => {
     const [selectedPain, setSelectedPain] = useState("");
@@ -34,18 +35,18 @@ const ChoosePain = () => {
                 מה כואב לחיה?
             </Typography>
             <div className='rectanglePain'>
-                <Button className="piece" onClick={() => handlePainClick('cut')}
-                        data-selected={selectedPain === 'Thermometer' ? 'true' : 'false'}>
+                <Button className="piece" onClick={() => handlePainClick('fiver')}
+                        data-selected={selectedPain === 'fiver' ? 'true' : selectedPain === "" ? 'default' : 'false'}>
                     <img src={ThermometerImagePath} style={{width: "100%", height: "100%"}} alt="Thermometer"/>
                     ?יש לה חום
                 </Button>
-                <Button className="piece" onClick={() => handlePainClick('fiver')}
-                        data-selected={selectedPain === 'Plaster' ? 'true' : 'false'}>
+                <Button className="piece" onClick={() => handlePainClick('cut')}
+                        data-selected={selectedPain === 'cut' ? 'true' : selectedPain === "" ? 'default' : 'false'}>
                     <img src={PlasterImagePath} style={{width: "100%", height: "100%"}} alt="Plaster"/>
                     ?יש לה חתך
                 </Button>
             </div>
-            <Button id="ContinueButton" disabled={selectedPain === ""} onClick={handleContinueClick}>
+            <Button id="ContinueButton" disabled={selectedPain === null} onClick={handleContinueClick}>
                 המשך
             </Button>
         </div>
--- src/Pages/Game/GamePage.jsx
+++ src/Pages/Game/GamePage.jsx
@@ -2,15 +2,17 @@ import './GamePage.css'
 import CustomToolbar from "../../Components/CustomToolbar.jsx";
 import * as React from "react";
 import { useLocation } from 'react-router-dom';
-import {Button} from "@mui/material";
+import { Button } from "@mui/material";
 import Dice from '../../Components/Dice';
-import {useState} from "react";
-
-
+import { useState } from "react";
+import vetLionImagePath from "../../../Public/HomePage/VetLion.png";
+import vetPandaImagePath from "../../../Public/HomePage/VetPanda.png";
+import vetMonkeyImagePath from "../../../Public/HomePage/VetMonkey.png";
+import vetDogImagePath from "../../../Public/HomePage/VetDog.png";
 
 const GamePage = () => {
     const location = useLocation();
-    const { pain, animal } = location.state || {};
+    const {pain, animal} = location.state || {};
     const [diceNumber, setDiceNumber] = useState(1);
 
     console.log("Pain: ", pain, "Animal: ", animal)
@@ -20,19 +22,42 @@ const GamePage = () => {
         setDiceNumber(newDiceNumber);
     };
 
+    const animalClass = `animal ${animal || ''}`; // Use an empty string as a fallback if 'animal' is not provided
+
+    let animalImage;
+    switch (animal) {
+        case 'dog':
+            animalImage = vetDogImagePath;
+            break;
+        case 'lion':
+            animalImage = vetLionImagePath;
+            break;
+        case 'monkey':
+            animalImage = vetMonkeyImagePath;
+            break;
+        case 'panda':
+            animalImage = vetPandaImagePath;
+            break;
+        default:
+            animalImage = null;
+    }
+
     return (
         <div className="container">
             <div className="toolbar-container">
                 <CustomToolbar toolbarTitle="סולמות ומגלשות"/>
             </div>
             <div className="game-board">
-                {Array.from({ length: 100 }, (_, index) => (
-                    <div className="cell" key={index} />
+                {Array.from({length: 100}, (_, index) => (
+                    <div className="cell" key={index}/>
                 ))}
             </div>
-            <div className="animal background">
-                <Dice diceNumber={diceNumber}/>
-                <Button id="RollButton" onClick={rollDice}>הטל</Button>
+            <div className={animalClass}>
+                {animalImage && <img src={animalImage} alt={animal} className="animal-image"/>}
+                <div className='rectangle'>
+                    <Dice diceNumber={diceNumber}/>
+                    <Button id="RollButton" onClick={rollDice}>הטל</Button>
+                </div>
             </div>
         </div>
     );
