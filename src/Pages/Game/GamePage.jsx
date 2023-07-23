422f625b6d8388b2ecc6995e7ddf60d7aef622e1
salit-av <salit.avramovich@gmail.com>
2023-07-23 15:47:25 +0300

    Slides and ladders added

--- src/Pages/Game/GamePage.jsx
+++ src/Pages/Game/GamePage.jsx
@@ -7,7 +7,8 @@ import vetLionImagePath from "../../../Public/HomePage/VetLion.png";
 import vetPandaImagePath from "../../../Public/HomePage/VetPanda.png";
 import vetMonkeyImagePath from "../../../Public/HomePage/VetMonkey.png";
 import vetDogImagePath from "../../../Public/HomePage/VetDog.png";
-import slideImagePath from "../../../Public/GamePage/water-slide-04 1.png";
+import slideImagePath from "../../../Public/GamePage/water-slide.png";
+import ladderImagePath from "../../../Public/GamePage/ladder.png";
 
 const GamePage = () => {
     const location = useLocation();
@@ -53,11 +54,28 @@ const GamePage = () => {
                         </div>
                     );
                 })}
+
                 <div className="slide-image1">
                     <img src={slideImagePath} alt="Slide" className="slide-image1" />
                 </div>
                 <div className="slide-image2">
                     <img src={slideImagePath} alt="Slide" className="slide-image2" />
+                </div>
+                <div className="slide-image3">
+                    <img src={slideImagePath} alt="Slide" className="slide-image3" />
+                </div>
+
+                <div className="ladder-image1">
+                    <img src={ladderImagePath} alt="Slide" className="ladder-image1" />
+                </div>
+                <div className="ladder-image2">
+                    <img src={ladderImagePath} alt="Slide" className="ladder-image2" />
+                </div>
+                <div className="ladder-image3">
+                    <img src={ladderImagePath} alt="Slide" className="ladder-image3" />
+                </div>
+                <div className="ladder-image4">
+                    <img src={ladderImagePath} alt="Slide" className="ladder-image4" />
                 </div>
             </div>
 
