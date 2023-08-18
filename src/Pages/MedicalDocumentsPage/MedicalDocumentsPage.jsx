0fe7618e2fc9a142fb0febd09b7851001396b8cf
Oz Hayun <ozhayun2@gmail.com>
2023-08-18 17:08:37 +0300

    fix Medical Files direction and add the pdfs for watch and downlaod

--- src/Pages/MedicalDocumentsPage/MedicalDocumentsPage.jsx
+++ src/Pages/MedicalDocumentsPage/MedicalDocumentsPage.jsx
@@ -4,12 +4,13 @@ import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
 import ListItemText from '@mui/material/ListItemText';
 import ArrowDownwardIcon from '@mui/icons-material/ArrowDownward';
+import RemoveRedEyeIcon from '@mui/icons-material/RemoveRedEye';
 import IconButton from '@mui/material/IconButton';
 import CustomToolbar from '../../Components/CustomToolbar.jsx';
 
 const pdfFiles = [
+    { title: "תהליך הטיפול במיון", filename: "emergency_room_treatment.pdf" },
     { title: "הדבקת חתך", filename: "adhesive_cut.pdf" },
-    { title: "הטיפול במיון", filename: "emergency_room_treatment.pdf" },
     { title: "טיפול בחבלת ראש", filename: "head_injury_treatment.pdf" },
     { title: "טיפול בחום", filename: "fever_treatment.pdf" }
 ];
@@ -27,21 +28,37 @@ const MedicalDocumentsPage = () => {
             <List sx={{ width: '100%' }}>
                 {pdfFiles.map((file, index) => (
                     <ListItem
+                        className="ListItem"
                         key={file.filename}
                         disableGutters
-                        secondaryAction={
-                            <a href={`${MedicalFilesPagePath}/${file.filename}`} download={file.title + ".pdf"}>
-                                <IconButton aria-label="download">
-                                    <ArrowDownwardIcon />
-                                </IconButton>
-                            </a>
-                        }
                         sx={{
                             backgroundColor: index % 2 === 1 ? 'inherit' : '#f5f5f5',
                         }}
                     >
-                        <ListItemText primary={file.title} />
+                        <ListItemText className="ListItemText" primary={file.title} />
+
+                        <div className="icon-container">
+                            {/* Open in a new tab */}
+                            <a href={`${MedicalFilesPagePath}/${file.filename}`}
+                               target="_blank"
+                               rel="noopener noreferrer">
+                                <IconButton className="button" aria-label="open">
+                                    <RemoveRedEyeIcon />
+                                </IconButton>
+                            </a>
+
+                            {/* Download */}
+                            <a href={`${MedicalFilesPagePath}/${file.filename}`}
+                               download={file.title + ".pdf"}>
+                                <IconButton className="button" aria-label="download">
+                                    <ArrowDownwardIcon />
+                                </IconButton>
+                            </a>
+                        </div>
                     </ListItem>
+
+
+
                 ))}
             </List>
         </div>
