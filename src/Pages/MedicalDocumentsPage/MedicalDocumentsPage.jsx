0563753d8d59fc720fd8a36abff70011069828f5
Oz Hayun <ozhayun2@gmail.com>
2023-08-22 11:36:26 +0300

    puhs to check pdf on production

--- src/Pages/MedicalDocumentsPage/MedicalDocumentsPage.jsx
+++ src/Pages/MedicalDocumentsPage/MedicalDocumentsPage.jsx
@@ -15,7 +15,7 @@ const pdfFiles = [
     { title: "טיפול בחום", filename: "fever_treatment.pdf" }
 ];
 
-const MedicalFilesPagePath = "/Public/MedicalFilesPage";
+const MedicalFilesPagePath = `${process.env.PUBLIC_URL}/Public/MedicalFilesPage`;
 
 const MedicalDocumentsPage = () => {
 
