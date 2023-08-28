eda8b0799d2efd751a0b93a2775d08b37b5c095f
salit-av <salit.avramovich@gmail.com>
2023-08-28 20:21:10 +0300

    abort change in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,33 +6,21 @@ import MedicalDocumentsPage from './Pages/MedicalDocumentsPage/MedicalDocumentsP
 import ChooseAnimal from './Pages/Game/ChooseAnimal'
 import ChoosePain from './Pages/Game/ChoosePain';
 import GamePage from './Pages/Game/GamePage';
-import ButtonClickSound from '/Public/Sounds/ClickSound.mp3';
+
 
 function App() {
     return (
-        <Router>
-            <Routes>
-                <Route path="/" element={<HomePage />} />
-                <Route path="/medical-documents" element={<MedicalDocumentsPage />} />
-                <Route path="/game" element={<GamePage />} />
-                <Route
-                    path="/choose-animal"
-                    element={
-                    <ChooseAnimal
-                        playClickSound={playClickSound}
-                    />
-                    }
-                />
-                <Route
-                    path="/choose-pain"
-                    element={
-                    <ChoosePain
-                        playClickSound={playClickSound}
-                     />
-                    }
-                />
-            </Routes>
-        </Router>
+        <SoundProvider>
+            <Router>
+                <Routes>
+                    <Route path="/" element={<HomePage />} />
+                    <Route path="/medical-documents" element={<MedicalDocumentsPage />} />
+                    <Route path="/game" element={<GamePage />} />
+                    <Route path="/choose-animal" element={<ChooseAnimal/>}/>
+                    <Route path="/choose-pain" element={<ChoosePain/>}/>
+                </Routes>
+            </Router>
+        </SoundProvider>
     );
 }
 
