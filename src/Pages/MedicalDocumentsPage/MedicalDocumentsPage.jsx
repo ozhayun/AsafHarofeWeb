7ff06d4fe4c3fa56e5232eb79edf0cdea9b2a61e
salit-av <salit.avramovich@gmail.com>
2023-08-28 23:11:20 +0300

    Change medical doc from pdf to jpg

--- src/Pages/MedicalDocumentsPage/MedicalDocumentsPage.jsx
+++ src/Pages/MedicalDocumentsPage/MedicalDocumentsPage.jsx
@@ -1,24 +1,29 @@
-import './MedicalDocumentsPage.css'
-import React from 'react';
+import './MedicalDocumentsPage.css';
+import React, { useState } from 'react';
 import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
 import ListItemText from '@mui/material/ListItemText';
 import ArrowDownwardIcon from '@mui/icons-material/ArrowDownward';
 import RemoveRedEyeIcon from '@mui/icons-material/RemoveRedEye';
 import IconButton from '@mui/material/IconButton';
+import Modal from '@mui/material/Modal';
 import CustomToolbar from '../../Components/CustomToolbar.jsx';
 
 const pdfFiles = [
-    { title: "תהליך הטיפול במיון", filename: "emergency_room_treatment.pdf" },
-    { title: "הדבקת חתך", filename: "adhesive_cut.pdf" },
-    { title: "טיפול בחבלת ראש", filename: "head_injury_treatment.pdf" },
-    { title: "טיפול בחום", filename: "fever_treatment.pdf" }
+    { title: "תהליך הטיפול במיון", filename: "emergency_room_treatment.jpg" },
+    { title: "הדבקת חתך", filename: "adhesive_cut.jpg" },
+    { title: "טיפול בחבלת ראש", filename: "head_injury_treatment.jpg" },
+    { title: "טיפול בחום", filename: "fever_treatment.jpg" }
 ];
 
 const MedicalFilesPagePath = `Public/MedicalFilesPage`;
 
 const MedicalDocumentsPage = () => {
+    const [selectedFile, setSelectedFile] = useState(null);
 
+    const openModal = (file) => {
+        setSelectedFile(file);
+    };
 
     return (
         <div className="medical documents container">
@@ -38,28 +43,31 @@ const MedicalDocumentsPage = () => {
                         <ListItemText className="ListItemText" primary={file.title} />
 
                         <div className="icon-container">
-                            {/* Open in a new tab */}
-                            <a href={`${MedicalFilesPagePath}/${file.filename}`}
-                               target="_blank"
-                               rel="noopener noreferrer">
-                                <IconButton className="button" aria-label="open">
-                                    <RemoveRedEyeIcon />
-                                </IconButton>
-                            </a>
-
-                            {/* Download */}
-                            <a href={`${MedicalFilesPagePath}/${file.filename}`}
-                               download={file.title + ".pdf"}>
-                                <IconButton className="button" aria-label="download">
-                                    <ArrowDownwardIcon />
-                                </IconButton>
-                            </a>
+                            {/* Open in modal */}
+                            <IconButton className="button" aria-label="open" onClick={() => openModal(file)}>
+                                <RemoveRedEyeIcon />
+                            </IconButton>
                         </div>
                     </ListItem>
                 ))}
             </List>
-        </div>
 
+            <Modal
+                open={selectedFile !== null}
+                onClose={() => setSelectedFile(null)}
+                aria-labelledby="image-modal"
+                aria-describedby="image-modal-description"
+            >
+                <div className="modal-content">
+                    {selectedFile && (
+                        <img
+                            src={`${MedicalFilesPagePath}/${selectedFile.filename}`}
+                            alt={selectedFile.title}
+                        />
+                    )}
+                </div>
+            </Modal>
+        </div>
     );
 }
 
