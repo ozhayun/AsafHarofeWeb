a955e328d49b3dde61d21a32aa355cd8d5ee5ab4
Oz Hayun <ozhayun2@gmail.com>
2023-08-30 16:36:29 +0300

    Another pdf check

--- src/Pages/MedicalDocumentsPage/MedicalDocumentsPage.jsx
+++ src/Pages/MedicalDocumentsPage/MedicalDocumentsPage.jsx
@@ -7,60 +7,64 @@ import ArrowDownwardIcon from '@mui/icons-material/ArrowDownward';
 import RemoveRedEyeIcon from '@mui/icons-material/RemoveRedEye';
 import IconButton from '@mui/material/IconButton';
 import CustomToolbar from '../../Components/CustomToolbar.jsx';
+import treatmentPdf from '../../../Public/MedicalFilesPage/emergency_room_treatment.pdf';
+import adhesiveCutPdf from '../../../Public/MedicalFilesPage/adhesive_cut.pdf';
+import headInjuryPdf from '../../../Public/MedicalFilesPage/head_injury_treatment.pdf';
+import feverPdf from '../../../Public/MedicalFilesPage/fever_treatment.pdf';
 
 const pdfFiles = [
-    { title: "תהליך הטיפול במיון", filename: "emergency_room_treatment.pdf" },
-    { title: "הדבקת חתך", filename: "adhesive_cut.pdf" },
-    { title: "טיפול בחבלת ראש", filename: "head_injury_treatment.pdf" },
-    { title: "טיפול בחום", filename: "fever_treatment.pdf" }
+    { title: "תהליך הטיפול במיון", filepath: treatmentPdf },
+    { title: "הדבקת חתך", filepath: adhesiveCutPdf },
+    { title: "טיפול בחבלת ראש", filepath: headInjuryPdf },
+    { title: "טיפול בחום", filepath: feverPdf }
 ];
 
 const MedicalFilesPagePath = `/MedicalFilesPage`;
 
 const MedicalDocumentsPage = () => {
 
+
     return (
         <div className="medical documents container">
             <div className="toolbar-container">
                 <CustomToolbar toolbarTitle="דפי הסבר רפואים"/>
             </div>
-            <List className="files-list">
-                {pdfFiles.map((file, index) => {
+            <List className="files-list" >
+                {pdfFiles.map((file, index) => (
+                    <ListItem
+                        className="ListItem"
+                        key={file.filepath}
+                        disableGutters
+                        sx={{
+                            backgroundColor: index % 2 === 1 ? 'inherit' : '#f5f5f5',
+                        }}
+                    >
+                        <ListItemText className="ListItemText" primary={file.title} />
 
-                    // Log the file path here
-                    console.log(`Trying to access: ${MedicalFilesPagePath}/${file.filename}`);
+                        <div className="icon-container">
+                            {/* Open in a new tab */}
+                            <a href={file.filepath}
+                               target="_blank"
+                               rel="noopener noreferrer">
+                                <IconButton className="button" aria-label="open">
+                                    <RemoveRedEyeIcon />
+                                </IconButton>
+                            </a>
 
-                    return (
-                        <ListItem
-                            className="ListItem"
-                            key={file.filename}
-                            // ... other props
-                        >
-                            {/* ... */}
-                            <div className="icon-container">
-                                <a href={`${MedicalFilesPagePath}/${file.filename}`}
-                                   target="_blank"
-                                   rel="noopener noreferrer">
-                                    <IconButton className="button" aria-label="open">
-                                        <RemoveRedEyeIcon />
-                                    </IconButton>
-                                </a>
-                                {/* Download */}
-                                <a href={`${MedicalFilesPagePath}/${file.filename}`}
-                                   download={file.title + ".pdf"}>
-                                    <IconButton className="button" aria-label="download">
-                                        <ArrowDownwardIcon />
-                                    </IconButton>
-                                </a>                            </div>
-                        </ListItem>
-                    );
-                })}
+                            {/* Download */}
+                            <a href={file.filepath}
+                               download={file.title + ".pdf"}>
+                                <IconButton className="button" aria-label="download">
+                                    <ArrowDownwardIcon />
+                                </IconButton>
+                            </a>
+                        </div>
+                    </ListItem>
+                ))}
             </List>
         </div>
 
     );
 }
-
-
 
 export default MedicalDocumentsPage;
