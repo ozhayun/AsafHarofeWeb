4d62b4af2bed69b801a6ad2dde2b8c2b6df2c29b
Oz Hayun <ozhayun2@gmail.com>
2023-08-30 16:06:51 +0300

    Trying to back to pdf

--- src/Pages/MedicalDocumentsPage/MedicalDocumentsPage.jsx
+++ src/Pages/MedicalDocumentsPage/MedicalDocumentsPage.jsx
@@ -1,34 +1,29 @@
-import './MedicalDocumentsPage.css';
-import React, { useState } from 'react';
+import './MedicalDocumentsPage.css'
+import React from 'react';
 import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
 import ListItemText from '@mui/material/ListItemText';
 import ArrowDownwardIcon from '@mui/icons-material/ArrowDownward';
 import RemoveRedEyeIcon from '@mui/icons-material/RemoveRedEye';
 import IconButton from '@mui/material/IconButton';
-import Modal from '@mui/material/Modal';
 import CustomToolbar from '../../Components/CustomToolbar.jsx';
 
 const pdfFiles = [
-    { title: "תהליך הטיפול במיון", filename: "emergency_room_treatment.jpg" },
-    { title: "הדבקת חתך", filename: "adhesive_cut.jpg" },
-    { title: "טיפול בחבלת ראש", filename: "head_injury_treatment.png" },
-    { title: "טיפול בחום", filename: "fever_treatment.jpg" }
+    { title: "תהליך הטיפול במיון", filename: "emergency_room_treatment.pdf" },
+    { title: "הדבקת חתך", filename: "adhesive_cut.pdf" },
+    { title: "טיפול בחבלת ראש", filename: "head_injury_treatment.pdf" },
+    { title: "טיפול בחום", filename: "fever_treatment.pdf" }
 ];
 
-const MedicalFilesPagePath = "../../../Public/MedicalFilesPage";
+const MedicalFilesPagePath = `/MedicalFilesPage`;
 
 const MedicalDocumentsPage = () => {
-    const [selectedFile, setSelectedFile] = useState(null);
 
-    const openModal = (file) => {
-        setSelectedFile(file);
-    };
 
     return (
         <div className="medical documents container">
             <div className="toolbar-container">
-                <CustomToolbar toolbarTitle="מסמכים רפואים"/>
+                <CustomToolbar toolbarTitle="דפי הסבר רפואים"/>
             </div>
             <List className="files-list" >
                 {pdfFiles.map((file, index) => (
@@ -43,31 +38,28 @@ const MedicalDocumentsPage = () => {
                         <ListItemText className="ListItemText" primary={file.title} />
 
                         <div className="icon-container">
-                            {/* Open in modal */}
-                            <IconButton className="button" aria-label="open" onClick={() => openModal(file)}>
-                                <RemoveRedEyeIcon />
-                            </IconButton>
+                            {/* Open in a new tab */}
+                            <a href={`${MedicalFilesPagePath}/${file.filename}`}
+                               target="_blank"
+                               rel="noopener noreferrer">
+                                <IconButton className="button" aria-label="open">
+                                    <RemoveRedEyeIcon />
+                                </IconButton>
+                            </a>
+
+                            {/* Download */}
+                            <a href={`${MedicalFilesPagePath}/${file.filename}`}
+                               download={file.title + ".pdf"}>
+                                <IconButton className="button" aria-label="download">
+                                    <ArrowDownwardIcon />
+                                </IconButton>
+                            </a>
                         </div>
                     </ListItem>
                 ))}
             </List>
-
-            <Modal
-                open={selectedFile !== null}
-                onClose={() => setSelectedFile(null)}
-                aria-labelledby="image-modal"
-                aria-describedby="image-modal-description"
-            >
-                <div className="modal-content">
-                    {selectedFile && (
-                        <img
-                            src={`${MedicalFilesPagePath}/${selectedFile.filename}`}
-                            alt={selectedFile.title}
-                        />
-                    )}
-                </div>
-            </Modal>
         </div>
+
     );
 }
 
