7ba0240534138afb1093ee6bbed54a710615adab
salit-av <salit.avramovich@gmail.com>
2023-09-03 15:51:42 +0300

    Add animalHebrew to CutBoard

--- src/Pages/Game/GamePage.jsx
+++ src/Pages/Game/GamePage.jsx
@@ -165,7 +165,8 @@ const GamePage = () => {
                 <CustomToolbar toolbarTitle="סולמות ומגלשות"/>
             </div>
             {pain === 'cut' ? (
-                <CutBoard playerPosition={playerPosition}
+                <CutBoard animalHebrew={animalHebrew}
+                          playerPosition={playerPosition}
                           playerImage={playerImage}
                           onLaddersChange={handleLaddersChange}
                           onSlidesChange={handleSlidesChange}
