bdf18f58d56ab2c5e2ac7f0fa8a353b70d267dab
salit-av <salit.avramovich@gmail.com>
2023-09-07 17:22:05 +0300

    Change size img player and boarder color on player pos

--- src/Components/Cell.jsx
+++ src/Components/Cell.jsx
@@ -2,8 +2,10 @@ import React from 'react'
 import "./Cell.css"
 
 const Cell = ({ playerPosition, playerImage, cellNumber }) => {
+    const isPlayerOnCell = cellNumber === playerPosition;
+
     return (
-        <div className="cell">
+        <div className={`cell ${isPlayerOnCell ? 'highlighted' : ''}`}>
             <span className="cell-number">{cellNumber}</span>
             {cellNumber === playerPosition && (
                 <img src={playerImage} alt="Player" className="player-image" />
